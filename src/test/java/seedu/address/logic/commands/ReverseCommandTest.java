92561e0a435e45f583ccd352c0850a5109b38c67
sciffany <sciffany@gmail.com>
2019-03-15 15:49:01 +0800

    rename var

--- src/test/java/seedu/address/logic/commands/ReverseCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ReverseCommandTest.java
@@ -6,6 +6,7 @@ import static seedu.address.testutil.TypicalCards.getTypicalTopDeck;
 import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -17,11 +18,13 @@ import seedu.address.model.UserPrefs;
 public class ReverseCommandTest {
 
     private final Model model = new ModelManager(getTypicalTopDeck(), new UserPrefs());
-    private final Model expectedModel = new ModelManager(getTypicalTopDeck(), new UserPrefs());
+//    private final Model expectedModel = new ModelManager(getTypicalTopDeck(), new UserPrefs());
     private final CommandHistory commandHistory = new CommandHistory();
 
     @Test
     public void execute() {
-        assertCommandFailure(new ReverseCommand(), model, commandHistory, UndoCommand.MESSAGE_FAILURE);
+        assertCommandFailure(new ReverseCommand(), model, commandHistory, ReverseCommand.MESSAGE_FAILURE);
     }
+
+
 }
